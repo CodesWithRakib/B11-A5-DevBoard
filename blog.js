1594693a7e10e7f03227436b7505903599a117a0
Md. Rakib Islam <r.s.rakib.business@gmail.com>
2025-03-02 10:19:28 +0600

    Updated some work

---
+++ blog.js
@@ -0,0 +1,5 @@
+/* Back to desk btn */
+let backToDeskBtn = document.getElementById("backToDesk");
+backToDeskBtn.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+/* Date Update in html using js */
 let dayMainName = document.getElementById("dayName");
 let localFullDate = document.getElementById("localFullDate");
 let date = document.getElementById("date");
 let newDay = new Date().toDateString();
 newDay = newDay.split(" ");
+//newDay = `${newDay[0]} , ${newDay[1]} ${newDay[2]} ${newDay[3]}`;
 let dayName = `${newDay[0]} ,`;
 let localDate = `${newDay[1]} ${newDay[2]} ${newDay[3]}`;
-newDay = `${newDay[0]} , ${newDay[1]} ${newDay[2]} ${newDay[3]}`;
 dayMainName.innerText = dayName;
 localFullDate.innerText = localDate;
-
+/* Discover Button related js */
+let discoverBtn = document.getElementById("discoverBtn");
+discoverBtn.addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+/* Task related js */
 let task = document.getElementById("task");
 let totalTask = document.getElementById("total-task");
 let collectBtn = document.querySelectorAll(".collect-btn");
@@ -17,8 +23,8 @@ for (let i = 0; i < collectBtn.length; i++) {
   collectBtn[i].addEventListener("click", function () {
     alert("Board Updated Successfully");
     collectBtn[i].setAttribute("disabled", true);
-    collectBtn[i].style.backgroundColor = "grey";
-    collectBtn[i].style.color = "black";
+    collectBtn[i].style.backgroundColor = "#dee2e6";
+    collectBtn[i].style.color = "#adb5bd";
     task.innerText = parseInt(task.innerText) - 1;
     totalTask.innerText = parseInt(totalTask.innerText) + 1;
     let newActivity = document.createElement("p");
@@ -28,9 +34,23 @@ for (let i = 0; i < collectBtn.length; i++) {
         .innerText;
     newActivity.innerText = `You have Complete The Task ${taskTitle} at ${new Date().toLocaleTimeString()}`;
     activity.appendChild(newActivity);
+    if (task.innerText == 0) {
+      alert("Congrats ! You have completed all the task for today");
+    }
   });
 }
 let clearBtn = document.getElementById("clear-history");
 clearBtn.addEventListener("click", function () {
   activity.innerHTML = "";
+});
+
+/* Background Change */
+
+let themeBtn = document.getElementById("themeBtn");
+themeBtn.addEventListener("click", function () {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let bgColor = `rgb(${red},${green},${blue})`;
+  document.body.style.backgroundColor = bgColor;
 });
